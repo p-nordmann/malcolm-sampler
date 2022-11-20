b449c10bdfe23cd8a040c8d31622306974db2715
Pierre-Louis Nordmann <pierrelouis.nordmann@gmail.com>
2022-11-20 20:27:09 +0000

    fix: typo

--- cmd/malcolms/help.go
+++ cmd/malcolms/help.go
@@ -1,7 +1,7 @@
 /*
 Malcolms provides a gRPC API that exposes Malcolm's Sampler functionalities.
 
-IT is designed to meet the following requirements:
+It is designed to meet the following requirements:
   - allow to register bounding boxes and true posterior samples of variable sizes and dimensions.
   - allow to generate samples from them in a concurrent fashion.
 
